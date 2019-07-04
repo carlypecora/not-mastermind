9137d025ec3b98831260e2619495385b2b9543e9
Carly Pecora <carly@carlypecora.com>
2019-07-04 11:53:44 -0400

    styling

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -3,13 +3,17 @@ import { View, Text } from 'react-native'
 import { StyleSheet } from 'react-native';
 
 
-export default class ColorSlots extends React.Component {
+export default class ColorPicker extends React.Component {
 
 	render(){
 		return(
-			<View style={{backgroundColor: 'gray'}} >
-				<Text className="picker">Here go the colors</Text>
-			</View>
+				<View style={{backgroundColor: 'gray', marginTop: 0, flex: 4, width: 375}}>
+					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+				</View>
 		)
 	}
 }
--- components/ColorSlots.js
+++ components/ColorSlots.js
@@ -5,7 +5,7 @@ export default class ColorSlots extends React.Component {
 
 	render(){
 		return(
-			<View style={{flex: 7, marginTop: 20}}>
+			<View style={{flex: 2, marginTop: 20, marginBottom: 20}}>
 				<Text>Hello World</Text>
 			</View>
 		)
