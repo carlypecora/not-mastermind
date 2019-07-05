9f0982da2fc60a2c995f9c553a8811c3a975bb09
Carly Pecora <carly@carlypecora.com>
2019-07-05 11:21:44 -0400

    styling

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -7,12 +7,13 @@ export default class ColorPicker extends React.Component {
 
 	render(){
 		return(
-				<View style={{backgroundColor: 'gray', marginTop: 0, flex: 4, width: 375}}>
-					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
-					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
-					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
-					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
-					<View style={{backgroundColor: 'white', borderRadius: 100, display: 'inline', position: 'absolute', height: 40, width: 40, marginLeft: 10, marginTop: 5}}></View>
+				<View style={{backgroundColor: 'gray', marginTop: 0, flex: 4, width: 375, flexDirection: 'row'}}>
+					<View style={{backgroundColor: 'white', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'hotpink', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'yellow', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'green', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'red', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
+					<View style={{backgroundColor: 'blue', borderRadius: 100, height: 50, width: 50, marginLeft: 10, marginTop: 5}}></View>
 				</View>
 		)
 	}
--- components/ColorSlots.js
+++ components/ColorSlots.js
@@ -5,8 +5,12 @@ export default class ColorSlots extends React.Component {
 
 	render(){
 		return(
-			<View style={{flex: 2, marginTop: 20, marginBottom: 20}}>
-				<Text>Hello World</Text>
+			<View style={{flex: 2, marginTop: 5, marginBottom: 20, flexDirection: 'row'}}>
+				<View style={{backgroundColor: 'white', borderRadius: 100, height: 40, width: 40, marginLeft: 20, marginTop: 5, float: 'right'}}></View>
+				<View style={{backgroundColor: 'white', borderRadius: 100, height: 40, width: 40, marginLeft: 20, marginTop: 5, float: 'right'}}></View>
+				<View style={{backgroundColor: 'white', borderRadius: 100, height: 40, width: 40, marginLeft: 20, marginTop: 5, float: 'right'}}></View>
+				<View style={{backgroundColor: 'white', borderRadius: 100, height: 40, width: 40, marginLeft: 20, marginTop: 5, float: 'right'}}></View>
+				<View style={{backgroundColor: 'lightgrey', borderRadius: 10, height: 50, width: 80, marginLeft: 20, marginTop: 5, float: 'right'}}></View>
 			</View>
 		)
 	}
